d7b20ab8d931e4fbb6446dc4a500213a04b587d4
yngridAMatos <matosingridy76@gmail.com>
2026-02-04 15:19:23 -0300

    Feat: Projeto Spring Boot

---
+++ src/main/java/com/example/ConnectFlow/ConnectFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConnectFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ConnectFlow/ConnectFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConnectFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
